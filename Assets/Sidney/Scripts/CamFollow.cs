7c722fe8952da59978a1e1b6c3a09b7328be624b
cpu-man <71815606+cpu-man@users.noreply.github.com>
2024-03-07 10:44:16 +0100

    11

---
+++ Assets/Sidney/Scripts/CamFollow.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public GameObject atachedCar;
+    public GameObject CamFolder;
+    public Transform[] camLocations;
+    public 
+}
